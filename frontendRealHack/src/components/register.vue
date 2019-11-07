f2606b6da50fe94f4f055c11a4f8494a2f995343
Ikshura <Ikshura Tachintha@BullEt.anoyms.lcl>
2019-11-07 22:51:07 +0530

    register form

--- frontendRealHack/src/components/register.vue
+++ frontendRealHack/src/components/register.vue
@@ -1,34 +1,36 @@
 <template>
- <div class="col-sm-6">
+ <div class="col-sm-12">
     <div class="card text-center  " style="width: 28rem;">
 <form>
   <h5 class="card-header info-color white-text text-center py-4">
         <strong>Register</strong>
     </h5>
   <div class="form-colum">
-     <div class="form-group mx-sm-3 mb-2 ">
-    <label for="FullName">Full Name</label>
-    <input 
-     type="text"
-         
-          
-          name="name"
-          placeholder="name"
-          v-model="register.name"
-     class="form-control" id="inputAddress" >
-  </div>
-    <div class="form-group mx-sm-3 mb-2 ">
-      <label for="inputEmail4">Email</label>
+  <div class="form-group mx-sm-3 mb-2 ">
+      <label for="inputEmail4"></br>Email</label>
       <input  
           
           name="login"
          
-          v-model="register.email" type="email" class="form-control" id="inputEmail4" placeholder="Email">
+          v-model="register.email" type="email" class="form-control" id="inputEmail4" placeholder="Enter email">
           <small
             id="emailHelp"
             class="form-text text-muted"
           >We'll never share your email with anyone else.</small>
     </div>
+    </br>
+     <div class="form-group mx-sm-3 mb-2 ">
+    <label for="FullName">User name</label>
+    <input 
+     type="text"
+         
+          
+          name="name"
+          placeholder="Enter a user name"
+          v-model="register.name"
+     class="form-control" id="inputAddress" >
+  </div>
+    </br>
     <div class="form-group mx-sm-3 mb-2">
       <label for="inputPassword4">Password</label>
       <input
@@ -40,11 +42,14 @@
        type="password" class="form-control" id="inputPassword4" placeholder="Password">
     </div>
   </div>
- 
+ </br></br>
  
   <button type="submit" class="btn btn-primary" @click="registerUser">Register</button>
 </form>
 
+    </div>
+    <div class="alert alert-info" role="alert">
+      Already have an account? <a href="#" class="alert-link">Sign in</a>.
     </div>
  </div>
   
